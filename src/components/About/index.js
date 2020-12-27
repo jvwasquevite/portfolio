8bc07d6bd51cb8337267231a9c7f2c2819e2c1ee
João <jvwasquevite@gmail.com>
2020-12-27 02:30:45 -0300

    mobile version adjustments

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import './style.css'
 
 import Parser from 'html-react-parser'
 import { useTranslation } from 'react-i18next'
+
+import SocialIcons from '../helpers/SocialIcons'
 
 const About = () => {
   const { t } = useTranslation()
@@ -11,6 +14,9 @@ const About = () => {
       <p className="sub-header">{t('head.subheader')}</p>
       <h1>{Parser(t('head.header'))}</h1>
       <p>{Parser(t('head.content'))}</p>
+      <div class="social">
+        <SocialIcons />
+      </div>
     </div>
   )
 }
