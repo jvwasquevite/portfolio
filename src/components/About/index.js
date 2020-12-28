8f3ea770aa9cd3f4eccff7a48d98befc75eb6db9
João <jvwasquevite@gmail.com>
2020-12-28 16:14:46 -0300

    fixed language switcher

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,7 +14,7 @@ const About = () => {
       <p className="sub-header">{t('head.subheader')}</p>
       <h1>{Parser(t('head.header'))}</h1>
       <p>{Parser(t('head.content'))}</p>
-      <div class="social">
+      <div className="social">
         <SocialIcons />
       </div>
     </div>
--- src/components/helpers/LangSwitcher/index.js
+++ src/components/helpers/LangSwitcher/index.js
@@ -6,7 +6,7 @@ const LangSwitcher = () => {
 
   return (
     <div id="LangSwitcher">
-      {i18n.language === 'pt' ? (
+      {i18n.language === 'pt' || i18n.language === 'pt-BR' ? (
         <button onClick={() => i18n.changeLanguage('en')}>EN-US</button>
       ) : (
         <button onClick={() => i18n.changeLanguage('pt')}>PT-BR</button>
