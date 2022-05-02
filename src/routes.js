81f62e4fe8903a2e7569701770b009eae53d38a6
João Wasquevite <jvwasquevite@gmail.com>
2022-05-02 20:04:03 -0300

    updated router

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from './containers/Home'
 import Doce22 from './containers/Doce22'
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <Router baseline="/">
       <Switch>
         <Route component={Home} exact path="/" />
         <Route component={Doce22} exact path="/doce22" />
       </Switch>
-    </BrowserRouter>
+    </Router>
   )
 }
 
