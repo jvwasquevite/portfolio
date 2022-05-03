5b9c99068e0f3c99027bb1e9f03be251fa805107
João Wasquevite <jvwasquevite@gmail.com>
2022-05-02 21:26:49 -0300

    updated routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from './containers/Home'
 import Doce22 from './containers/Doce22'
 
 const Routes = () => {
   return (
-    <Router baseline="/">
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route component={Home} exact path="/" />
         <Route component={Doce22} exact path="/doce22" />
