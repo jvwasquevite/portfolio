40fe315bcf3c4a8378d9aabd3b31e963d36eb0b8
João Wasquevite <jvwasquevite@gmail.com>
2024-03-28 07:16:02 -0300

    removed outdated containers

--- src/containers/Doce22/Tinder/Logo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import logo from './assets/logo.svg'
-
-const Logo = () => {
-  return <img src={logo} className="logo-icon" alt="Tinder" />
-}
-
-export default Logo
--- src/containers/Doce22/Tinder/services/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL: 'https://tinder.jvwasquevite.com',
-  headers: { 'Access-Control-Allow-Origin': '*' },
-})
