b0ded78a6dc550ba091971f4e6f3da0c1af22ac3
João Wasquevite <jvwasquevite@gmail.com>
2024-03-28 08:36:04 -0300

    refactor: cursor pointer on language switcher

--- src/components/helpers/LangSwitcher/index.js
+++ src/components/helpers/LangSwitcher/index.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
+import React from "react"
+import "./styles.css"
+
+import { useTranslation } from "react-i18next"
 
 const LangSwitcher = () => {
   const { i18n } = useTranslation()
 
   return (
     <div id="LangSwitcher">
-      {i18n.language === 'pt' || i18n.language === 'pt-BR' ? (
-        <button onClick={() => i18n.changeLanguage('en')}>EN-US</button>
+      {i18n.language === "pt" || i18n.language === "pt-BR" ? (
+        <button onClick={() => i18n.changeLanguage("en")}>EN-US</button>
       ) : (
-        <button onClick={() => i18n.changeLanguage('pt')}>PT-BR</button>
+        <button onClick={() => i18n.changeLanguage("pt")}>PT-BR</button>
       )}
     </div>
   )
